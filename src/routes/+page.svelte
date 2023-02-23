fa94d37d79ef42a34add30d76754fc1c26e63eba
Noah <70114451+zFl4wless@users.noreply.github.com>
2023-02-23 16:11:21 +0100

    small refactor and cool little feature (#11)
    * hotfix: fixes broken card layout design on chrome
    
    * fix: fixes random operator select
    
    * feat: adds operator select transitions for mobile
    
    * feat: adds desktop select operator transitions
    
    * feat: adds cards hover effect
    
    * feat: adds basic opengraph meta tags
    
    * hotfix: remove unused import
    
    * chore: updates outdated packages
    
    * feat: makes search bar sticky
    
    * feat: adds shadows and aligns operator cards left
    
    * feat: adds credits to myself :)
    
    * style: adds smooth gradient under search bar
    
    * style: improves footer placement
    
    * refactor: removes unused reactives
    
    * feat: unselectes operator when pressing escape

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,9 +7,7 @@
 	import SplashScreen from '$lib/components/SplashScreen.svelte';
 	import { onMount } from 'svelte';
 
-	let showSplashScreen: boolean;
-	$: showSplashScreen = true;
-
+	let showSplashScreen: boolean = true;
 	onMount(() => {
 		showSplashScreen = false;
 	});
@@ -21,7 +19,13 @@
 			: operators;
 
 	let selectedOperator: Operator | undefined;
-	$: selectedOperator;
+	onMount(() => {
+		window.addEventListener('keydown', (event) => {
+			if (event.key === 'Escape') {
+				selectedOperator = undefined;
+			}
+		});
+	});
 </script>
 
 <main class="mx-6 flex min-h-screen">
